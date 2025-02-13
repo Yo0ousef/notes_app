5d067358a436d76eb2fb021544b3e5960b5ac36c
Yousef <youssefmansour594@gmail.com>
2025-02-13 11:23:54 +0200

    added a circular floating action button

--- lib/views/news_view.dart
+++ lib/views/news_view.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
 
-
 class NewsView extends StatelessWidget {
   const NewsView({super.key});
+
   static String id = 'newsview';
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: NotesViewBody() ,
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+       backgroundColor: Colors.lightBlueAccent,
+        foregroundColor: Colors.black,
+        shape: CircleBorder(),
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      body: NotesViewBody(),
     );
   }
 }
-
