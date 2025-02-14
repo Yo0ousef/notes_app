c419606f141c679a3b8ea86d4e8ce324f5641212
Yousef <youssefmansour594@gmail.com>
2025-02-14 20:35:31 +0200

    added bottom Modal sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
 
 class NewsView extends StatelessWidget {
   const NewsView({super.key});
 
-  static String id = 'newsview';
+  static String id = 'notesView';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-       backgroundColor: Colors.lightBlueAccent,
+        backgroundColor: Colors.lightBlueAccent,
         foregroundColor: Colors.black,
         shape: CircleBorder(),
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              });
+        },
         child: Icon(Icons.add),
       ),
       body: NotesViewBody(),
     );
   }
 }
+
--- lib/widgets/note_list_view.dart
+++ lib/widgets/note_list_view.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/note_item.dart';
+
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (context , index){
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          child: NoteItem(),
-        );
-      },);
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: ListView.builder(
+        padding: EdgeInsets.zero,
+        itemCount: 10,
+        itemBuilder: (context , index){
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: NoteItem(),
+          );
+        },),
+    );
   }
 }
